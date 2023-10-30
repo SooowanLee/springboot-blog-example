25d01edda8e30fd7699149968d2b43679c2fcc21
Soowan Lee <lsy9406@gmail.com>
2023-10-30 20:37:44 +0900

    add 유저 저장 메서드 작성

--- src/main/java/me/leesoowan/springbootdeveloper/service/UserService.java
+++ src/main/java/me/leesoowan/springbootdeveloper/service/UserService.java
@@ -1,2 +1,25 @@
-package me.leesoowan.springbootdeveloper.service;public class UserService {
+package me.leesoowan.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.leesoowan.springbootdeveloper.domain.User;
+import me.leesoowan.springbootdeveloper.dto.AddUserRequest;
+import me.leesoowan.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                //패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
 }

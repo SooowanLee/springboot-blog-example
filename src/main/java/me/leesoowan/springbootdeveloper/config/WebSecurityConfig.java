64b905c82cefd77577531248d01861ed2b26c01f
Soowan Lee <lsy9406@gmail.com>
2023-10-30 19:58:39 +0900

    add 스프링시큐리티 UserDetailsService를 상속받고 메서드 오버라이딩 하였음"

---
+++ src/main/java/me/leesoowan/springbootdeveloper/config/WebSecurityConfig.java
@@ -0,0 +1,2 @@
+package me.leesoowan.springbootdeveloper.config;public class WebSecurityConfig {
+}
--- src/main/java/me/leesoowan/springbootdeveloper/service/UserDetailService.java
+++ src/main/java/me/leesoowan/springbootdeveloper/service/UserDetailService.java
@@ -1,2 +1,22 @@
-package me.leesoowan.springbootdeveloper.service;public class UserDetailService {
+package me.leesoowan.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.leesoowan.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+//스프링 시큐리티에서 사용자 정보를 가져오는 인터페이스
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    //사용자 이름(email)으로 사용자의 정보를 가져오는 메서드
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
 }

dd24dce8b2d63e0a2b8e360a72d43f90e3445e70
Soowan Lee <lsy9406@gmail.com>
2023-10-30 20:19:13 +0900

    add User객체를 저장하는 메서드 작성

--- src/main/java/me/leesoowan/springbootdeveloper/controller/UserViewController.java
+++ src/main/java/me/leesoowan/springbootdeveloper/controller/UserViewController.java
@@ -1,2 +1,18 @@
-package me.leesoowan.springbootdeveloper.controller;public class UserViewController {
+package me.leesoowan.springbootdeveloper.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
 }
--- src/main/java/me/leesoowan/springbootdeveloper/service/UserService.java
+++ src/main/java/me/leesoowan/springbootdeveloper/service/UserService.java
@@ -1,2 +1,24 @@
-package me.leesoowan.springbootdeveloper.service;public class UserService {
+package me.leesoowan.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.leesoowan.springbootdeveloper.domain.User;
+import me.leesoowan.springbootdeveloper.dto.AddUserRequest;
+import me.leesoowan.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+
 }

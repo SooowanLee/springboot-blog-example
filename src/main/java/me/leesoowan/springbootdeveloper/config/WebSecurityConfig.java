7707fcaf40ae5897a2826616b2756b7eeebb2fd9
Soowan Lee <lsy9406@gmail.com>
2023-10-31 14:03:51 +0900

    add filterChafix(), authenticationManager() 스프링시큐리티6 문법으로 수정

--- src/main/java/me/leesoowan/springbootdeveloper/config/WebSecurityConfig.java
+++ src/main/java/me/leesoowan/springbootdeveloper/config/WebSecurityConfig.java
@@ -5,52 +5,57 @@ import me.leesoowan.springbootdeveloper.service.UserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private final UserDetailService userDetailService;
+    private final UserDetailService userService;
 
     //스프링 시큐리티 기능 비활성화
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console())
-                .requestMatchers("/static/**");
+                .requestMatchers(new AntPathRequestMatcher("/static/**"));
     }
 
     //특정 HTTP 요청에 대한 웹 기반 보안 구성
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeRequests() //인증, 인가 설정
-                .requestMatchers("/login", "/signup", "/user").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin((formLogin) -> formLogin.loginPage("/login")
-                        .defaultSuccessUrl("/articles")) //폼 기반 로그인 설정
-                .logout((logout) -> logout.logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true))   //로그아웃 설정
-                .csrf((csrf) -> csrf.disable())  //csrf 비활성화
+        return http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/login"),
+                                new AntPathRequestMatcher("/signup"),
+                                new AntPathRequestMatcher("/user")).permitAll()
+                        .anyRequest().authenticated()) // 인증, 인가 설정
+                .formLogin((login) -> login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/articles")) // 폼 기반 로그인 설정
+                .logout((logout) -> logout
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)) // 로그아웃 설정
+                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
                 .build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http,
-                                                       BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailService)
-                .passwordEncoder(bCryptPasswordEncoder)
-                .and()
-                .build();
+    public AuthenticationManager authenticationManager(BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userService);
+        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+        return new ProviderManager(authenticationProvider);
     }
 
     //패스워드 인코더로 사용할 빈 등록
@@ -58,5 +63,4 @@ public class WebSecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }

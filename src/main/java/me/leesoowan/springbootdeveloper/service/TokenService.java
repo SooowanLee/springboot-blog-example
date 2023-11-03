b17af267f34233a51d158298c06637093ac1c3b6
Soowan Lee <lsy9406@gmail.com>
2023-11-03 17:11:06 +0900

    add refreshToken 생성 메서드 추가

--- src/main/java/me/leesoowan/springbootdeveloper/service/TokenService.java
+++ src/main/java/me/leesoowan/springbootdeveloper/service/TokenService.java
@@ -1,2 +1,29 @@
-package me.leesoowan.springbootdeveloper.service;public class TokenService {
+package me.leesoowan.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.leesoowan.springbootdeveloper.config.jwt.TokenProvider;
+import me.leesoowan.springbootdeveloper.domain.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final UserService userService;
+
+    public String createNewAccessToken(String refreshToken) throws IllegalAccessException {
+        //토큰 유효성 검사에 실패하면 예외 발생
+        if (!tokenProvider.validToken(refreshToken)) {
+            throw new IllegalAccessException("Unexpected token");
+        }
+
+        Long userId = refreshTokenService.findByRefreshToken(refreshToken).getUserId();
+        User user = userService.findById(userId);
+
+        return tokenProvider.generateToken(user, Duration.ofHours(2));
+    }
 }

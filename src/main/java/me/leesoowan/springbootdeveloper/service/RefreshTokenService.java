821cba6797146559e7a25fb1496f478fcd6405e5
Soowan Lee <lsy9406@gmail.com>
2023-11-03 17:09:09 +0900

    add refreshToken 검색 기능 추가

--- src/main/java/me/leesoowan/springbootdeveloper/service/RefreshTokenService.java
+++ src/main/java/me/leesoowan/springbootdeveloper/service/RefreshTokenService.java
@@ -1,2 +1,17 @@
-package me.leesoowan.springbootdeveloper.service;public class RefreshTokenService {
+package me.leesoowan.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.leesoowan.springbootdeveloper.domain.RefreshToken;
+import me.leesoowan.springbootdeveloper.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow((() -> new IllegalArgumentException("Unexpected token")));
+    }
 }

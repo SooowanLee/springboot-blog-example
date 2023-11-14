8ac535914386d832b8d2e40d167233cf18c6c724
Soowan Lee <lsy9406@gmail.com>
2023-11-14 15:27:54 +0900

    fix BlogService updateArticle() -> update()변경

--- src/main/java/me/leesoowan/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/leesoowan/springbootdeveloper/controller/BlogApiController.java
@@ -55,7 +55,7 @@ public class BlogApiController {
 
     @PutMapping("/api/articles/{id}")
     public ResponseEntity<Article> updateArticle(@PathVariable long id, @RequestBody UpdateArticleRequest request) {
-        Article updatedArticle = blogService.updateArticle(id, request);
+        Article updatedArticle = blogService.update(id, request);
 
         return ResponseEntity.ok()
                 .body(updatedArticle);

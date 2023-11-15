748dca714858f9fe501b472c3636e88bef5c5e3d
Soowan Lee <lsy9406@gmail.com>
2023-11-16 01:29:16 +0900

    add 없는 게시물 조회시 에러

--- src/main/java/me/leesoowan/springbootdeveloper/config/error/exception/ArticleNotFoundException.java
+++ src/main/java/me/leesoowan/springbootdeveloper/config/error/exception/ArticleNotFoundException.java
@@ -1,2 +1,9 @@
-package me.leesoowan.springbootdeveloper.config.error.exception;public class ArticleNotFoundException {
+package me.leesoowan.springbootdeveloper.config.error.exception;
+
+import me.leesoowan.springbootdeveloper.config.error.ErrorCode;
+
+public class ArticleNotFoundException extends NotFoundException {
+    public ArticleNotFoundException() {
+        super(ErrorCode.ARTICLE_NOT_FOUND);
+    }
 }
